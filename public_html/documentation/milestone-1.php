5334270d0ec6835d4007c209369d366943abfee9
James R. Lambert <ryolambert@gmail.com>
2018-07-15 18:20:41 -0600

    added basic file directory structure.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 1</title>
+		<link rel="stylesheet"
+				type="text/css"
+				href="style.css"/>
+	</head>
+	<body>
+		<?php
+
+
+
+		?>
+	</body>
+</html>
