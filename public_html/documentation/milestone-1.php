5d1d905368a0023e396405b4f929293e3eda7dca
James R. Lambert <ryolambert@gmail.com>
2018-07-18 10:28:38 -0600

    added pwp milestone one page, outlines purpose, audience, goals, and creates a persona.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,16 +2,127 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css"
+				href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Oswald|Space+Mono" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="./style.css"/>
 		<title>PWP Milestone 1</title>
-		<link rel="stylesheet"
-				type="text/css"
-				href="style.css"/>
 	</head>
 	<body>
-		<?php
+		<div class="container">
+				<h1><u>PWP Milestone One</u></h1>
+				<div class="well col-xs-12 col-md-12 col-lg-12 box-shadow">
+					<div>
+						<h2 id="purposeaudiencegoalanswers">Purpose, Audience, Goal Answers</h2>
+
+						<ol>
+							<li><p><strong>Purpose</strong>: To put on display the passion I have for the creative applications of
+									technology. As well as, showcase unique talents and skills as a fullstack web developer, front-end
+									engineer, and designer. </p></li>
+
+							<li><p><strong>Audience</strong>: Potential- Employers, Startup Business Partners, Clients.</p></li>
+
+							<li><p><strong>Goal</strong>: <br/>
+									<em>Main Goal</em></p>
+
+								<ul>
+									<li>To acquire an internship/employment as a front-end web developer in ABQ.</li>
+								</ul>
+
+								<p><em>Sub Goals</em></p>
+
+								<ul>
+									<li>To attract freelance clients.</li>
+
+									<li>To serve as a gallery to showcase and promote my work.</li>
+
+									<li>To attract other local developers in town for collaboration on projects/startups.</li>
+								</ul>
+							</li>
+						</ol>
+
+						<hr/>
+					</div>
+				</div>
+			</div>
+				<p></p>
+			<div class="container">
+				<div class="well col-xs-12 col-md-12 col-lg-12 box-shadow">
+						<h2><i class="fa fa-user"></i></span> <u>Persona</u></h2>
+
+						<img src="marcus-sparkes.jpg" class="center-block box-shadow" alt="Headshot of Marcus Sparkes"/>
+
+						<h3 id="marcussparkes"><em>Marcus Sparkes</em></h3>
+
+						<p>39 years old, Male</p>
+
+						<p><strong>Profession:</strong></p>
+
+						<ul>
+							<li>Lead developer and department head of a rapidly growing mid-sized web/app development agency in
+								Albuquerque, NM
+							</li>
+						</ul>
+
+						<p><strong>Technology:</strong></p>
+
+						<ul>
+							<li>2018 15" MacBook Pro</li>
+
+							<li>34" Dell Ultrawide Quad HD Monitor</li>
+
+							<li>27" Dell P2715Q</li>
+
+							<li>Ubuntu Server 18.04 LTS</li>
+
+							<li>Iphone X</li>
+
+							<li>Apple Watch</li>
+						</ul>
+
+						<p><strong>Attitudes and Behaviors:</strong></p>
+
+						<p>Organized, meticulous, and fast-paced. Heavily focused on getting client product designed, tested, and pushed
+							into production on time and at or above spec. Takes a results-based approach to work and hiring. He relies
+							primarily on portfolios, Github, and referrals from colleagues in the industry. </p>
+
+						<p>Makes it a point to attend local developer events on a quarterly to biannual basis, and on occasion hosts local
+							hackathons or Geeks Who Drink event in town. During his free time, he is an avid photographer and loves taking
+							his dogs on hikes up through the Chama and Pecos. </p>
+
+						<p><strong>Frustrations and Needs:</strong></p>
+
+						<p>Marc has been apart of the technology sector for almost two decades now and has been involved with projects and
+							companies across the US and abroad.
+							Marc has been apart of the technology sector for almost two decades now and has been involved with projects and
+							companies across the US and abroad. His biggest issue in working with and hiring new employees is finding
+							individuals who are hungry to learn, expand upon their skillsets, and unafraid to apply their creative passions
+							in the work they do. For the position, he is looking to hire someone who's going to go above and beyond the
+							bare minimum, with a penchant for intuitive designs and innovative solutions. As well as a willingness to
+							undergo rapid personal and professional growth over the next 3-5 years while at the company. </p>
+
+						<p><strong>Goals:</strong></p>
+
+						<p>Marc is looking to hire a junior full-stack developer with two to three years experience. His company is
+							setting a rapid pace and sets to take on another dozen major clients in the next quarter alone. He hopes to
+							start taking on at least 1-2 developers quickly. </p>
+
+						<p>Right now, he has taken time away as project lead and is reviewing applications, referrals, and sending out
+							interview requests to potential hires. After seeing my application, portfolio site, and reviewing a few
+							repositories on Github he is ready to shoot me an email for an interview. </p>
+
+						<p><strong>User Story:</strong></p>
+
+						<p>As a user, I need to determine quickly whether this candidate is qualified for the full-stack developer
+							position.</p>
+				</div>
+			</div>
 
 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-		?>
 	</body>
 </html>
