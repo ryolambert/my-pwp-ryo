8e6f06b470ccc7c9ec1f1175f93a4366967120e4
James R. Lambert <ryolambert@gmail.com>
2018-08-29 23:40:23 -0600

    added mailer config, debugging AOS.js disappearing on mobile...

---
+++ public_html/php/mail-config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * mail-config.php
+ * This file contains your reCAPTCHA API keys and your recipient's email addresses.
+ *
+ * @param string $siteKey your public reCAPTCHA API key
+ * @param string $secret your secret reCAPTCHA API key
+ * @param array $MAIL_RECIPIENTS array of email addresses and corresponding recipient names to send form responses to
+ *
+ * @author Rochelle Lewis <rlewis37@cnm.edu>
+ *
+ * This file contains sensitive information and should ALWAYS be gitignored!
+ **/
+
+// your Google reCAPTCHA keys here
+$siteKey = '-- reCAPTCHA SITE KEY --';
+$secret = '-- reCAPTCHA SECRET KEY --';
+
+/**
+ * attach the recipients to the message
+ * notice this an array that can include or omit the the recipient's real name
+ * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+ **/
+$MAIL_RECIPIENTS = ["mail@email.com" => "Recipient Name"];
