68ba388b306040374c8545040f09ae3a3f3c2755
Ryo Lambert <ryolambert@gmail.com>
2018-11-07 15:09:12 -0700

    fixed mailer setup so the ajaxHandler verifies and pops up success or error prompts within the email contact card element instead of rerouting to a new page with the plain text. 🤦‍♂️

--- public_html/js/form-validate.js
+++ public_html/js/form-validate.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	 **/
 
 	/* begin validate function here */
-	$("#contact-form").validate({
+	$("#form").validate({
 
 		// setup handling of form errors
 		debug: true,
@@ -55,9 +55,9 @@ $(document).ready(function(){
 
 		// AJAX submit the form data to back end if rules pass
 		submitHandler: function(form) {
-			$("#contact-form").ajaxSubmit({
+			$("#form").ajaxSubmit({
 				type: "POST",
-				url: $("#contact-form").attr("action"),
+				url: $("#form").attr("action"),
 
 				success: function(ajaxOutput) {
 					// clear the output area's formatting
@@ -68,7 +68,7 @@ $(document).ready(function(){
 
 					// reset the form if it was successful
 					if($(".alert-success").length >= 1) {
-						$("#contact-form")[0].reset();
+						$("#form")[0].reset();
 					}
 				}
 			})
